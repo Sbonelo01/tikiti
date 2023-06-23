0fd7f32ceb0d6d0a1e8fab481a506a9e383ee10d
Sbonelo01 <sbonelomkhize15@gmail.com>
2023-06-23 20:35:43 +0200

    Updates

---
+++ precache-manifest.af02194b7f52207b5a4c8de190683f55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33c8501947dfd6b5272d56f4094d1b5",
+    "url": "/tikiti/index.html"
+  },
+  {
+    "revision": "18a352d50adb8f3e8d91",
+    "url": "/tikiti/static/css/2.ca8e03c8.chunk.css"
+  },
+  {
+    "revision": "0b1d94630338b358777a",
+    "url": "/tikiti/static/css/main.b095f89a.chunk.css"
+  },
+  {
+    "revision": "18a352d50adb8f3e8d91",
+    "url": "/tikiti/static/js/2.a96e465b.chunk.js"
+  },
+  {
+    "revision": "0b1d94630338b358777a",
+    "url": "/tikiti/static/js/main.08b3fc2a.chunk.js"
+  },
+  {
+    "revision": "4baf41ed26eaf8004796",
+    "url": "/tikiti/static/js/runtime~main.ce8f1a4a.js"
+  },
+  {
+    "revision": "bbdffecce22743b0c58c95acf27d4afd",
+    "url": "/tikiti/static/media/logo.bbdffecc.svg"
+  }
+]);
