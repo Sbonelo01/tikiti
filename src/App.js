2cbd63451b2485d284405f50d61b95b60b67e02c
Sbonelo01 <sbonelomkhize15@gmail.com>
2023-10-21 16:30:15 +0200

    comment out bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
