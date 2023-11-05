04d52aa1fa14ba5dd4850a208eb9b69971b368f5
Sbonelo01 <sbonelomkhize15@gmail.com>
2023-11-05 15:57:40 +0200

    tailwind and daisy ui works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 import { isMobile } from "react-device-detect";
 
@@ -10,53 +10,146 @@ import { ProductConsumer } from "../context";
 export default class Navbar extends Component {
   render() {
     return (
-      <NavWrapper className="navbar nav-bar-expand-sm navbar-dark px-sm-5">
-        <Link to="/">
-          <img
-            width={isMobile ? 50 : 100}
-            src={logo}
-            alt="store"
-            className="navbar-brand"
-          />
-        </Link>
-        <Link to="/cart">
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "auto auto",
-              padding: "60px",
-            }}
-          >
-            <div>
-              <i className="fas fa-cart-plus"></i>
-            </div>
+      // <NavWrapper class="navbar nav-bar-expand-sm navbar-dark px-sm-5">
+      //   <Link to="/">
+      //     <img
+      //       width={isMobile ? 50 : 100}
+      //       src={logo}
+      //       alt="store"
+      //       class="navbar-brand"
+      //     />
+      //   </Link>
+      //   <Link to="/cart">
+      //     <div
+      //       style={{
+      //         display: "grid",
+      //         gridTemplateColumns: "auto auto",
+      //         padding: "60px",
+      //       }}
+      //     >
+      //       <div>
+      //         <i class="fas fa-cart-plus"></i>
+      //       </div>
+      //       <ProductConsumer>
+      //         {(amount) => {
+      //           return (
+      //             <div style={{ paddingLeft: "10px" }}>
+      //               <p>{`R ${amount.cartTotal}`}</p>
+      //             </div>
+      //           );
+      //         }}
+      //       </ProductConsumer>
+      //     </div>
+      //   </Link>
+      // </NavWrapper>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link to="/">
+            <img
+              width={isMobile ? 50 : 100}
+              src={logo}
+              alt="store"
+              class="navbar-brand"
+            />
+          </Link>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
             <ProductConsumer>
               {(amount) => {
+                // return <>{amount.cartTotal}</>;
+                // }}
                 return (
-                  <div style={{ paddingLeft: "10px" }}>
-                    <p>{`R ${amount.cartTotal}`}</p>
-                  </div>
+                  <>
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                      <div className="indicator">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                          />
+                        </svg>
+                        {console.log(amount)}
+                        <span className="badge badge-sm indicator-item">
+                          {amount.cart.length}
+                        </span>
+                      </div>
+                    </label>
+                    <div
+                      tabIndex={0}
+                      className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+                    >
+                      <div className="card-body">
+                        <span className="font-bold text-lg">
+                          {amount.cart.length} tickets
+                        </span>
+                        <span className="text-info">
+                          Subtotal: {amount.cartTotal}
+                        </span>
+                        <div className="card-actions">
+                          <Link to="/cart">
+                            <button className="btn btn-primary btn-block">
+                              View cart
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </>
                 );
               }}
             </ProductConsumer>
           </div>
-        </Link>
-      </NavWrapper>
+
+          {/* <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div> */}
+        </div>
+      </div>
     );
   }
 }
-const NavWrapper = styled.nav`
-  background: var(--mainWhite);
-  width: 100%;
-  top: 0;
-  position: fixed;
-  border-top: 0px solid gray;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  margin-bottom: 100px;
-  z-index: 5;
-  .nav-link {
-    color: var(--mainWhite) !important;
-    font-size: 1.3 rem;
-    text-transform: capitalize;
-  }
-`;
+// const NavWrapper = styled.nav`
+//   background: var(--mainWhite);
+//   width: 100%;
+//   top: 0;
+//   position: fixed;
+//   border-top: 0px solid gray;
+//   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+//   margin-bottom: 100px;
+//   z-index: 5;
+//   .nav-link {
+//     color: var(--mainWhite) !important;
+//     font-size: 1.3 rem;
+//     text-transform: capitalize;
+//   }
+// `;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
 import PropTypes from "prop-types";
@@ -7,49 +7,84 @@ import PropTypes from "prop-types";
 export default class Product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product;
-    console.log(img);
+
     return (
-      <ProducrWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-        <div className="card">
+      // <ProducrWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+      //   <div className="card">
+      //     <ProductConsumer>
+      //       {(value) => (
+      //         <div
+      //           className="img-container p-5"
+      //           onClick={() => value.handleDetail(id)}
+      //         >
+      //           <Link to="/details">
+      //             <img src={img} alt="product" className="card-img-top" />
+      //           </Link>
+      //           <button
+      //             className="cart-btn"
+      //             disabled={inCart ? true : false}
+      //             onClick={() => {
+      //               value.addToCart(id);
+      //               value.openModal(id);
+      //             }}
+      //           >
+      //             {inCart ? (
+      //               <Link to="/cart">
+      //                 <p className="text-capitalize mb-0" disabled>
+      //                   {""}pay
+      //                 </p>
+      //               </Link>
+      //             ) : (
+      //               <i className="fas fa-shopping-cart" />
+      //             )}
+      //           </button>
+      //         </div>
+      //       )}
+      //     </ProductConsumer>
+      //     <div className="card-footer d-flex justify-content-between">
+      //       <p className="align-self-center mb-0">{title}</p>
+      //       <h5 className="mb-0">
+      //         <span className="mr-1">R</span>
+      //         {price}
+      //       </h5>
+      //     </div>
+      //   </div>
+      // </ProducrWrapper>
+      <>
+        <div className="card w-96 glass">
           <ProductConsumer>
-            {(value) => (
-              <div
-                className="img-container p-5"
-                onClick={() => value.handleDetail(id)}
-              >
-                <Link to="/details">
-                  <img src={img} alt="product" className="card-img-top" />
-                </Link>
-                <button
-                  className="cart-btn"
-                  disabled={inCart ? true : false}
-                  onClick={() => {
-                    value.addToCart(id);
-                    value.openModal(id);
-                  }}
-                >
-                  {inCart ? (
-                    <Link to="/cart">
-                      <p className="text-capitalize mb-0" disabled>
-                        {""}pay
-                      </p>
-                    </Link>
-                  ) : (
-                    <i className="fas fa-shopping-cart" />
-                  )}
-                </button>
-              </div>
-            )}
+            {(value) => {
+              return (
+                <>
+                  {console.log(value.detailProduct.info)}
+                  <div onClick={() => value.handleDetail(id)}>
+                    <figure>
+                      <img src={img} alt={`${img}${title}`} />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{title}</h2>
+                      <p>{value.detailProduct.info.slice(0, 20)}</p>
+                      <div className="card-actions justify-end">
+                        <button
+                          className={
+                            inCart ? "btn btn-secondary" : "btn btn-primary"
+                          }
+                          onClick={() => {
+                            value.addToCart(id);
+                            value.openModal(id);
+                          }}
+                        >
+                          Get ticket for {price}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            }}
           </ProductConsumer>
-          <div className="card-footer d-flex justify-content-between">
-            <p className="align-self-center mb-0">{title}</p>
-            <h5 className="mb-0">
-              <span className="mr-1">R</span>
-              {price}
-            </h5>
-          </div>
         </div>
-      </ProducrWrapper>
+      </>
     );
   }
 }
@@ -62,55 +97,55 @@ Product.propTypes = {
     inCart: PropTypes.bool,
   }).isRequired,
 };
-const ProducrWrapper = styled.div`
-  .card {
-    border-color: tranparent;
-    transition: all 1s linear;
-    border-radius: 15px;
-  }
-  .card-footer {
-    background: transparent;
-    border-top: transparent;
-    transition: all 1s linear;
-  }
-  &:hover {
-    .card {
-      border: 0.04rem solid rgba(0, 0, 0, 0.2);
-      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
-    }
-    .card-footer {
-      background: rgba(247, 247, 247);
-      border-radius: 0px 0px 15px 15px;
-    }
-  }
-  .img-container {
-    position: relative;
-    overflow: hidden;
-  }
-  .card-img-top {
-    transition: all 1s linear;
-  }
-  .img-container:hover .card-img-top {
-    transform: scale(1.2);
-  }
-  .cart-btn {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    border-radius: 50%;
-    padding: 0.2rem 0.4rem;
-    background: var(--lightBlue);
-    border: 0.1px solid transparent;
-    box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
-    color: var(--mainWhite);
-    font-size: 1.4rem;
-    transition: all 1s linear;
-  }
-  .img-container:hover .cart-btn {
-    transform: translate(0, 0);
-  }
-  .cart-btn:hover {
-    color: var(--mainBlue);
-    cursor: pointer;
-  }
-`;
+// const ProducrWrapper = styled.div`
+//   .card {
+//     border-color: tranparent;
+//     transition: all 1s linear;
+//     border-radius: 15px;
+//   }
+//   .card-footer {
+//     background: transparent;
+//     border-top: transparent;
+//     transition: all 1s linear;
+//   }
+//   &:hover {
+//     .card {
+//       border: 0.04rem solid rgba(0, 0, 0, 0.2);
+//       box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+//     }
+//     .card-footer {
+//       background: rgba(247, 247, 247);
+//       border-radius: 0px 0px 15px 15px;
+//     }
+//   }
+//   .img-container {
+//     position: relative;
+//     overflow: hidden;
+//   }
+//   .card-img-top {
+//     transition: all 1s linear;
+//   }
+//   .img-container:hover .card-img-top {
+//     transform: scale(1.2);
+//   }
+//   .cart-btn {
+//     position: absolute;
+//     bottom: 0;
+//     right: 0;
+//     border-radius: 50%;
+//     padding: 0.2rem 0.4rem;
+//     background: var(--lightBlue);
+//     border: 0.1px solid transparent;
+//     box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+//     color: var(--mainWhite);
+//     font-size: 1.4rem;
+//     transition: all 1s linear;
+//   }
+//   .img-container:hover .cart-btn {
+//     transform: translate(0, 0);
+//   }
+//   .cart-btn:hover {
+//     color: var(--mainBlue);
+//     cursor: pointer;
+//   }
+// `;
