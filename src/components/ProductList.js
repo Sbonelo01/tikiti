d3ef1ac46f1f751469c4156fe70f3bc5a9f4c7d0
Sbonelo01 <sbonelomkhize15@gmail.com>
2023-11-06 18:37:59 +0200

    context: state preserves after page reload

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,7 +3,9 @@ import Product from "./Product";
 import Title from "./Title";
 import { ProductConsumer } from "../context";
 import Footer from "./Footer";
+import DesktopFooter from "./DesktopFooter";
 import { isMobile } from "react-device-detect";
+import Hero from "./Hero";
 
 export default class ProductList extends Component {
   render() {
@@ -15,7 +17,7 @@ export default class ProductList extends Component {
     };
     const parentPhone = {
       display: "grid",
-      gridTemplateColumns: "100px",
+      gridTemplateColumns: "100%",
       gridColumnGap: "28%",
       gridRowGap: "28px",
     };
@@ -23,9 +25,69 @@ export default class ProductList extends Component {
     return (
       <React.Fragment>
         <div className="py-5">
+          {/* <Hero /> */}
           <div className="container">
             <Title name="" title="Tickets" />
-
+            <div>
+              <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                  <img
+                    src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide2" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                  <img
+                    src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide3" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                  <img
+                    src="/images/stock/photo-1414694762283-acccc27bca85.jpg"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide4" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                  <img
+                    src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+                    className="w-full"
+                  />
+                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">
+                      ❮
+                    </a>
+                    <a href="#slide1" className="btn btn-circle">
+                      ❯
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div style={isMobile ? parentPhone : parentDesktop}>
               <ProductConsumer>
                 {(value) => {
@@ -41,7 +103,7 @@ export default class ProductList extends Component {
             </div>
           </div>
           <br />
-          {isMobile ? <Footer /> : ""}
+          {isMobile ? <Footer /> : <DesktopFooter />}
         </div>
       </React.Fragment>
     );
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { isMobile } from "react-device-detect";
 
 export default function Title({ name, title }) {
   return (
-    <div className="row" style={{ marginTop: isMobile ? 40 : 80 }}>
+    <div className="row">
       <div className="col-10 mx-auto my-2 text-center text-title">
-        <h1
-          className="text-capitalize font-weight-bold"
-          style={{ paddingTop: 40 }}
-        >
+        <h1 className="text-capitalize font-weight-bold">
           {name} <strong className="text-blue">{title}</strong>
         </h1>
       </div>
